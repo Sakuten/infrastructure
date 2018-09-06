f84cd78e920b89b48d7998eb582e1f48ee446bea
coord.e <me@coord-e.com>
2018-09-06 23:50:04 +0900

    Change: Use 80 port in ecs

--- security_group.tf
+++ security_group.tf
@@ -50,8 +50,8 @@ resource "aws_security_group" "instance" {
 
   ingress {
     protocol  = "tcp"
-    from_port = 8080
-    to_port   = 8080
+    from_port = 80
+    to_port   = 80
 
     security_groups = ["${aws_security_group.lb.id}"]
   }
