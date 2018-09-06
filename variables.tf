38cb08b7420c93fad82d21e2f84cccd8b5e80798
coord.e <me@coord-e.com>
2018-09-07 00:01:35 +0900

    Add: Add missing variable declaration

--- variables.tf
+++ variables.tf
@@ -12,7 +12,24 @@ variable "regions" {
 
 variable "db_username" {}
 variable "db_password" {}
+variable "ssh_public_key" {}
 
 variable "admin_cidr_ingress" {
   default = "0.0.0.0/0"
+}
+
+variable "elb_health_check_endpoint" {
+  default = "/index.html"
+}
+
+variable "ec2_ami" {
+  default = "ami-0567c164"
+}
+
+variable "ec2_instance_type" {
+  default = "m4.medium"
+}
+
+variable "ec2_gp2_size" {
+  default = "30"
 }
