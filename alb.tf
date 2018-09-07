a8b8e2691689a3c7b3c1640e38e45867d6c85882
coord.e <me@coord-e.com>
2018-09-07 23:25:40 +0900

    Change: Separate ALB configuration

---
+++ alb.tf
@@ -0,0 +1,24 @@
+resource "aws_alb_target_group" "main" {
+  name     = "tf-${var.base_name}-ecs"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.main.id}"
+}
+
+resource "aws_alb" "main" {
+  name            = "tf-${var.base_name}-alb-ecs"
+  subnets         = ["${aws_subnet.main.*.id}"]
+  security_groups = ["${aws_security_group.lb_sg.id}"]
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.main.id}"
+    type             = "forward"
+  }
+}
+
--- main.tf
+++ main.tf
@@ -1,32 +1,6 @@
 
 data "aws_availability_zones" "available" {}
 
-## ALB
-
-resource "aws_alb_target_group" "main" {
-  name     = "tf-${var.base_name}-ecs"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = "${aws_vpc.main.id}"
-}
-
-resource "aws_alb" "main" {
-  name            = "tf-${var.base_name}-alb-ecs"
-  subnets         = ["${aws_subnet.main.*.id}"]
-  security_groups = ["${aws_security_group.lb_sg.id}"]
-}
-
-resource "aws_alb_listener" "front_end" {
-  load_balancer_arn = "${aws_alb.main.id}"
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = "${aws_alb_target_group.main.id}"
-    type             = "forward"
-  }
-}
-
 ## CloudWatch Logs
 
 resource "aws_cloudwatch_log_group" "ecs" {
