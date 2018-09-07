851a5b0e78e3a236bffeb11ac6c86a6d237c655c
coord.e <me@coord-e.com>
2018-09-07 23:24:06 +0900

    Change: Separate ECS configuration

---
+++ ecs.tf
@@ -0,0 +1,42 @@
+resource "aws_ecs_cluster" "main" {
+  name = "terraform_ecs_cluster"
+}
+
+data "template_file" "task_definition" {
+  template = "${file("${path.module}/task-definition.json")}"
+
+  vars {
+    image_url        = "389602053363.dkr.ecr.ap-northeast-1.amazonaws.com/sakuten:latest"
+    container_name   = "sakuten_backend"
+    log_group_region = "${var.aws_region}"
+    log_group_name   = "${aws_cloudwatch_log_group.app.name}"
+    secret_key = "${var.secret_key}"
+    recaptcha_secret_key = "${var.recaptcha_secret_key}"
+    database_url = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.db.endpoint}/postgres"
+  }
+}
+
+resource "aws_ecs_task_definition" "backend" {
+  family                = "tf_backend_td"
+  container_definitions = "${data.template_file.task_definition.rendered}"
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "tf-${var.base_name}-ecs"
+  cluster         = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.backend.arn}"
+  desired_count   = 1
+  iam_role        = "${aws_iam_role.ecs_service.name}"
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.main.id}"
+    container_name   = "sakuten_backend"
+    container_port   = "80"
+  }
+
+  depends_on = [
+    "aws_iam_role_policy.ecs_service",
+    "aws_alb_listener.front_end",
+  ]
+}
+
