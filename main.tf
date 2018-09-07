c7c3d398213ba0f758380be55da2c1f4123aaeda
coord.e <me@coord-e.com>
2018-09-07 16:51:52 +0900

    Fix: Use our unique name instead of ghost

--- main.tf
+++ main.tf
@@ -199,22 +199,22 @@ data "template_file" "task_definition" {
   }
 }
 
-resource "aws_ecs_task_definition" "ghost" {
-  family                = "tf_example_ghost_td"
+resource "aws_ecs_task_definition" "backend" {
+  family                = "tf_example_backend_td"
   container_definitions = "${data.template_file.task_definition.rendered}"
 }
 
 resource "aws_ecs_service" "test" {
-  name            = "tf-example-ecs-ghost"
+  name            = "tf-example-ecs-backend"
   cluster         = "${aws_ecs_cluster.main.id}"
-  task_definition = "${aws_ecs_task_definition.ghost.arn}"
+  task_definition = "${aws_ecs_task_definition.sakuten_backend.arn}"
   desired_count   = 1
   iam_role        = "${aws_iam_role.ecs_service.name}"
 
   load_balancer {
     target_group_arn = "${aws_alb_target_group.test.id}"
-    container_name   = "ghost"
-    container_port   = "2368"
+    container_name   = "sakuten_backend"
+    container_port   = "80"
   }
 
   depends_on = [
@@ -313,7 +313,7 @@ resource "aws_iam_role_policy" "instance" {
 ## ALB
 
 resource "aws_alb_target_group" "test" {
-  name     = "tf-example-ecs-ghost"
+  name     = "tf-example-ecs-backend"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.main.id}"
@@ -343,5 +343,5 @@ resource "aws_cloudwatch_log_group" "ecs" {
 }
 
 resource "aws_cloudwatch_log_group" "app" {
-  name = "tf-ecs-group/app-ghost"
+  name = "tf-ecs-group/app-backend"
 }
