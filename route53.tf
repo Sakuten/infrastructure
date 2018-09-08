329750a5e55ff6c0f7e7e477ce5e019133477ad5
coord.e <me@coord-e.com>
2018-09-08 20:03:51 +0900

    Change: Configure the frontend routing of Route53

--- route53.tf
+++ route53.tf
@@ -1,3 +1,19 @@
-resource "aws_route53_zone" "over_moe" {
+resource "aws_route53_zone" "sakuten" {
    name = "${var.domain}"
+}
+
+resource "aws_route53_record" "root" {
+   zone_id = "${aws_route53_zone.sakuten.zone_id}"
+   name = "${var.domain}"
+   type = "A"
+   ttl = "300"
+   records = ["104.198.14.52"]  // Netlify's load balancer
+}
+
+resource "aws_route53_record" "www" {
+   zone_id = "${aws_route53_zone.sakuten.zone_id}"
+   name = "www.${var.domain}"
+   type = "CNAME"
+   ttl = "300"
+   records = ["sakuten-deployment.netlify.com"]
 }
