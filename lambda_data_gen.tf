65136e5f79b03effa867ac1467b145900b76a5d5
coord.e <me@coord-e.com>
2018-09-09 03:01:19 +0900

    Add: Add lambda function configuration

--- lambda_data_gen.tf
+++ lambda_data_gen.tf
@@ -1,16 +1,34 @@
-data "external" "virtualenv_path" {
-  program = ["bash", "virtualenv_path.sh"]
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
+resource "aws_lambda_function" "gen_db" {
+  filename         = "dbgen/function.zip"
+  function_name    = "generate_initial_db"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "app.gen"
+  source_code_hash = "${base64sha256(file("dbgen/function.zip"))}"
+  runtime          = "python3.6"
 
-data "archive_file" "lambda_archive" {
-  type        = "zip"
-  source_dir  = "dbgen/"
-  output_path = "dbgen/function.zip"
-}
-
-data "archive_file" "lambda_archive_deps" {
-  type        = "zip"
-  source_dir  = "${data.external.virtualenv_path.result["path"]}/lib/python3.6/site-packages"
-  output_path = "dbgen/function.zip"
+  environment {
+    variables = {
+      DATABASE_URL = "${postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.db.endpoint}/postgres}"
+    }
+  }
 }
