a24b44648af2e07e70a35b483c55c15324442a07
coord.e <me@coord-e.com>
2018-09-09 02:46:24 +0900

    Change: Use archive_file to generate zip

--- lambda_data_gen.tf
+++ lambda_data_gen.tf
@@ -1,9 +1,16 @@
+data "external" "virtualenv_path" {
+  program = ["bash", "virtualenv_path.sh"]
+}
 
-data "template_file" "lambda_config_cfg" {
-  template = "${file("${path.module}/dbgen/config.tpl.cfg")}"
 
-  vars {
-    database_url = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.db.endpoint}/postgres"
-    ids_json_path = "../ids.json"
-  }
+data "archive_file" "lambda_archive" {
+  type        = "zip"
+  source_dir  = "dbgen/"
+  output_path = "dbgen/function.zip"
+}
+
+data "archive_file" "lambda_archive_deps" {
+  type        = "zip"
+  source_dir  = "${data.external.virtualenv_path.result["path"]}/lib/python3.6/site-packages"
+  output_path = "dbgen/function.zip"
 }
