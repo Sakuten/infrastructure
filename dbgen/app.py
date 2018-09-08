00442e6fa951a0a3c5f97048bf296efb362b16b3
coord.e <me@coord-e.com>
2018-09-09 01:34:32 +0900

    Change: Stop using config file and use environ

--- dbgen/app.py
+++ dbgen/app.py
@@ -1,11 +1,17 @@
 from backend.api.app import init_and_generate
+from backend.api.config import BaseConfig
+from backend.api.models import db
 from flask import Flask
 
 def gen():
     app = Flask(__name__)
-    app.config.from_pyfile('config.cfg')
+    app.config.from_object(BaseConfig)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['ID_LIST_FILE'] = './ids.json'
     app.config['DB_GEN_POLICY'] = 'always'
     app.config['DB_FORCE_INIT'] = True
+
+    db.init_app(app)
 
     with app.app_context():
         init_and_generate()
