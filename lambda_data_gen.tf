dbfd12c420cb8b566c009788f63df09a335eabf3
coord.e <me@coord-e.com>
2018-09-09 11:41:49 +0900

    Fix: Fix syntax mistakes

--- lambda_data_gen.tf
+++ lambda_data_gen.tf
@@ -36,7 +36,7 @@ EOF
 resource "aws_lambda_function" "gen_db" {
   filename         = "dbgen/function.zip"
   function_name    = "generate_initial_db"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  role             = "${aws_iam_role.lambda_iam.arn}"
   handler          = "app.gen"
   source_code_hash = "${base64sha256(file("dbgen/function.zip"))}"
   runtime          = "python3.6"
@@ -48,7 +48,7 @@ resource "aws_lambda_function" "gen_db" {
 
   environment {
     variables = {
-      DATABASE_URL = "${postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.db.endpoint}/postgres}"
+      DATABASE_URL = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.db.endpoint}/postgres"
     }
   }
 }
