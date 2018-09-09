e82862e4f9d8b4725b74021031dfd42e653d40bd
coord.e <me@coord-e.com>
2018-09-10 02:38:49 +0900

    Add: Generate a latest function.zip before deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd "$(dirname $0)/.."
+
+cd dbgen
+make install
+make
+cd ..
+
 epoch=$(date +%s)
 epoch_target=$((epoch + 60 * 10))
 read year month day hour minute < <(date -d @$epoch_target +'%-Y %-m %-d %-H %-M')
