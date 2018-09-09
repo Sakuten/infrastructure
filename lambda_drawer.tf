1c813c4a6a620c77d6909e9ace000a94d1f58193
coord.e <me@coord-e.com>
2018-09-09 18:20:46 +0900

    Add: Add cloudwatch events for each timepoints

---
+++ lambda_drawer.tf
@@ -0,0 +1,21 @@
+resource "aws_cloudwatch_event_rule" "tp" {
+  count               = 4
+  name                = "tf-${var.base_name}-tp${count.index}-event"
+  schedule_expression = "${element(var.tps, count.index)}"
+}
+
+resource "aws_cloudwatch_event_target" "tp_target" {
+  count     = 4
+  rule      = "${element(aws_cloudwatch_event_rule.tp, count.index).name}"
+  arn       = "${aws_lambda_function.drawer.arn}"
+}
+
+resource "aws_lambda_permission" "allow_call" {
+  count         = 4
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.drawer.function_name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${element(aws_cloudwatch_event_rule.tp, count.index).arn}"
+}
+
