5577d042f917509eb5adec01fa0edc796420f7e1
coord.e <me@coord-e.com>
2018-09-10 02:56:47 +0900

    Change: Rewrite exclude-destroy.sh using lib

--- scripts/exclude-destroy.sh
+++ scripts/exclude-destroy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+script_root=$(dirname $0)
+
+source $script_root/lib/schedule.sh
+
 exclude_targets=$(echo $@ | sed -e 's/\./\\./g' | tr ' ' '|')
 
+export TF_VAR_dbgen_schedule=$(calc_schedule $gen_extension)
 terraform destroy $(for r in `terraform state list | egrep -v "$exclude_targets"` ; do printf -- "-target ${r} "; done)
