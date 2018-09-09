515477a000a56d7a81d94c4708764e87f575a915
coord.e <me@coord-e.com>
2018-09-10 02:42:59 +0900

    Add: Add some progress outputs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+function info (){
+  echo -e "\033[0;32m-> \033[0m\033[0;01m $1\033[0;0m"
+}
+
 cd "$(dirname $0)/.."
+
+info "Generating function.zip"
 
 cd dbgen
 make install
 make
 cd ..
+
+info "Done"
 
 epoch=$(date +%s)
 epoch_target=$((epoch + 60 * 10))
@@ -13,4 +21,8 @@ read year month day hour minute < <(date -d @$epoch_target +'%-Y %-m %-d %-H %-M
 
 export TF_VAR_dbgen_schedule="cron($minute $hour $day $month ? $year)"
 
+info "DB Data generation is scheduled at: $year/$month/$day $hour:$minute"
+
 terraform apply
+
+info "All Done!"
