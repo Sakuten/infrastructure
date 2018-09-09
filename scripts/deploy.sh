98603063059986de0ea5d179554bce3168a026a0
coord.e <me@coord-e.com>
2018-09-10 02:37:03 +0900

    Change: export as var.dbgen_schedule

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ epoch=$(date +%s)
 epoch_target=$((epoch + 60 * 10))
 read year month day hour minute < <(date -d @$epoch_target +'%-Y %-m %-d %-H %-M')
 
-echo "cron($minute $hour $day $month ? $year)"
+export TF_VAR_dbgen_schedule="cron($minute $hour $day $month ? $year)"
