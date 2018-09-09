8fcb7a19eda43bd1c91023dab74b25da14355d0f
coord.e <me@coord-e.com>
2018-09-10 02:35:18 +0900

    Change: Use read builtin to get fields efficiently

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-function strip_num() {
-  echo $@ | sed -e 's/^0*\([0-9]\+\)$/\1/g'
-}
-
-
-year=$(date +%Y)
-month=$(strip_num $(date +%m))
-day=$(strip_num $(date +%d))
-
-hour=$(strip_num $(date +%H))
-minute=$(strip_num $(date +%M))
+read year month day hour minute < <(date +'%-Y %-m %-d %-H %-M')
 
 echo "cron($minute $hour $day $month ? $year)"
