b95e587a9478ea8930d267686ae29eb168134510
coord.e <me@coord-e.com>
2018-09-10 02:47:52 +0900

    Change: Enable to specify generation time extension from arg

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ function info (){
   echo -e "\033[0;32m-> \033[0m\033[0;01m $1\033[0;0m"
 }
 
+gen_extension=${1:-10}
+
 cd "$(dirname $0)/.."
 
 info "Generating function.zip"
@@ -16,12 +18,12 @@ cd ..
 info "Done"
 
 epoch=$(date +%s)
-epoch_target=$((epoch + 60 * 10))
+epoch_target=$((epoch + 60 * $gen_extension))
 read year month day hour minute < <(date -d @$epoch_target +'%-Y %-m %-d %-H %-M')
 
 export TF_VAR_dbgen_schedule="cron($minute $hour $day $month ? $year)"
 
-info "DB Data generation is scheduled at: $year/$month/$day $hour:$minute"
+info "DB Data generation is scheduled at: $year/$month/$day $hour:$minute (+$gen_extension)"
 
 terraform apply
 
