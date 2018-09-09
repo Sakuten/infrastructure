5d4c4931d125a6a76a488c756258941686225b67
coord.e <me@coord-e.com>
2018-09-10 02:39:10 +0900

    Add: Finally, apply!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,3 +12,5 @@ epoch_target=$((epoch + 60 * 10))
 read year month day hour minute < <(date -d @$epoch_target +'%-Y %-m %-d %-H %-M')
 
 export TF_VAR_dbgen_schedule="cron($minute $hour $day $month ? $year)"
+
+terraform apply
