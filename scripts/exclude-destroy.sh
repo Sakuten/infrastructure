348a19351d23265a5e2b8f0bd9bd57c20240c128
coord.e <me@coord-e.com>
2018-09-09 22:08:26 +0900

    Change: Simplify the script

--- scripts/exclude-destroy.sh
+++ scripts/exclude-destroy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 exclude_targets=$(echo $@ | sed -e 's/\./\\./g' | tr ' ' '|')
-tmpfile=/tmp/$(mktemp XXXXX.plan)
 
-terraform plan -destroy $(for r in `terraform state list | egrep -v "$exclude_targets"` ; do printf -- "-target ${r} "; done) -out $tmpfile
-terraform apply $tmpfile
+terraform destroy $(for r in `terraform state list | egrep -v "$exclude_targets"` ; do printf -- "-target ${r} "; done)
