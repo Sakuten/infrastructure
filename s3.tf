e7fdf8e0edd7f955a25261aa97fd3e738c997aff
coord.e <me@coord-e.com>
2019-07-15 06:56:28 +0000

    Fix: Replace md5(file( call with filemd5 to avoid non-UTF8 string

--- s3.tf
+++ s3.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket_object" "dbgen_archive" {
   bucket = "${aws_s3_bucket.bucket.id}"
   key    = "dbgen_archive"
   source = "${var.dbgen_archive_path}"
-  etag = "${md5(file("${var.dbgen_archive_path}"))}"
+  etag = "${filemd5("${var.dbgen_archive_path}")}"
 }
 
