5090a89dcd49ebdac5f5bd6e43647202401734ca
coord.e <me@coord-e.com>
2019-07-15 06:31:21 +0000

    Add: Add version constraint of providers

--- provider.tf
+++ provider.tf
@@ -2,4 +2,22 @@ provider "aws" {
   region = "${var.aws_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
+
+  version = "~> 2.7.0"
+}
+
+provider "acme" {
+  version = "~> 1.2.0"
+}
+
+provider "archive" {
+  version = "~> 1.2.0"
+}
+
+provider "template" {
+  version = "~> 2.1.0"
+}
+
+provider "tls" {
+  version = "~> 2.0.0"
 }
